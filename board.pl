c63ee9f6a2446dcfae4f25c535696589e017f56a
Esteban Osorio Gallardo <estebanyque@users.noreply.github.com>
2016-08-30 19:04:41 -0300

    useless
    initial to test to create a 2x2 array with each position.

--- board.pl
+++ board.pl
@@ -2,20 +2,20 @@
 
 # create a 3x3 board
 
-my %board = {
-	A => [1,2,3],
-	B => [1,2,3],
-	C => [1,2,3],
+#my %board = {
+	#A => [1,2,3],
+	#B => [1,2,3],
+	#C => [1,2,3],
 #	D => [1..8],
 #	E => [1..8],
 #	F => [1..8],
 #	G => [1..8],
 #	H => [1..8],
-	};
-foreach $row (keys %board) {
-	print "$row . \n";
+	#};
+#foreach $row (keys %board) {
+#	print "$row . \n";
 #	foreach $column (@$board{$row}) {
 #		print $row . $column . "\t"; 
 #	}
-	print "\n";
-}
+#	print "\n";
+#}
